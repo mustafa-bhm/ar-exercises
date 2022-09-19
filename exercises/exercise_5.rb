40f1bedf1df97a94f0f7674ebf0464673f3a7382
mustafa-bhm <mustafa.bhm3@gmail.com>
2022-09-19 18:59:17 +0000

    completed ex5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,19 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts "====== Total revenue ============"
+@total_revenue = Store.sum("annual_revenue")
+
+puts "total revenue is #{@total_revenue}"
+
+puts "====== Average revenue ============"
+@total_stores = Store.count
+
+@average_revenue = @total_revenue / @total_stores
+puts "Average annual revenue for all stores: $#{@average_revenue}"
+
+puts "===== number of stores that are generating $1M or more ====="
+@million_dollar_stores = Store.where('annual_revenue >= 1000000')
+@total_of_million_dollar_stores = @million_dollar_stores.count
+puts "Number of stores that are generating $1M or more in annual sales: #{@total_of_million_dollar_stores}"
